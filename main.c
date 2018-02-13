47edfc7cf1fccfa58c52477d53e5bea9daae903b
Daniel Robson <danielijrobson@gmail.com>
2018-02-13 23:28:37 +0000

    .

--- main.c
+++ main.c
@@ -3,31 +3,36 @@
 # include <unistd.h>
 # include <signal.h>
 # include <time.h>
+# include <string.h>
+# include <stdlib.h>
 mdl_u8_t lock = 0;
 void ctrl_c(int __sig) {
 	 lock = -1;
 }
 
-int main(void) {
+int main(int __argc, char const *__argv[]) {
+	if (__argc < 2) return -1;
+
 	signal(SIGINT, ctrl_c);
 	struct lat ts;
 	lat_prepare(&ts);
 
 	double period = 0.00;
 	double fi = 0.00;
-	mdl_u64_t const n = 0xfffff, start = 0;
+	mdl_u64_t n = strtol(__argv[1], NULL, 10), start = 0;
 	mdl_u64_t i = start;
 
 	while(i != n && !lock) {
 		lat_put(&ts, i, (void*)i);
 
 		if (period > 0.01) {
+			fi+=period;
 			printf("mem_usage: %lf mb, finished: %lf\n", (double)mem_usage()*0.000001, fi);
-			fi+= period;
 			period = 0;
-		} else
-			period+= (100.0/(double)n);
-		//usleep(1000);
+		}
+
+		period+= (100.0/(double)n);
+		usleep(10);
 		i++;
 	}
 
@@ -47,6 +52,7 @@ int main(void) {
 		i++;
 	}
 
+	lookup_time /= (double)n;
 	fprintf(stdout, "\nlookup time: %lfsec\n", lookup_time/1000000000.0);
 	fprintf(stdout, "records: %lu, mem_used: %lf mb\n", n, (double)mem_usage()*0.000001);
 
